000bdf3f1d8e7bab894b4291d776126d8c9530c3
leesanguk1013 <leesanguk1013@users.noreply.github.com>
2020-04-28 00:12:31 +0900

    UnityHW_01 (UI Test)
    UI Test _ MoveDice

---
+++ UnityHW_01/MoveDice/Assets/DirectionControll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionControll : MonoBehaviour
+{
+
+    public int DirectionNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+
+    void OnTriggerStay(Collider other)
+    {
+        
+
+        if (other.gameObject.tag == "Player")
+        {
+            Move.Direction_switcher = DirectionNum;
+
+        }
+    }
+    
+}
---
+++ UnityHW_01/MoveDice/Assets/Move.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour
+{
+    AudioSource BackMusic;
+    public Slider VolValue;
+    public GameObject Player;
+    public GameObject SoundSourceMan;
+    public RawImage Background_Onoff;
+    public bool BackOnoff = false;
+    public static int Direction_switcher = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackMusic = SoundSourceMan.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Background_Onoff.enabled = BackOnoff;
+    }
+
+    public void AudioSourceControll() {
+        BackMusic.volume = VolValue.value;
+    }
+
+    public void BackGrounds()
+    {
+       if( BackOnoff == true)
+            {
+            BackOnoff = false;
+        }
+        else if (BackOnoff == false)
+        {
+            BackOnoff = true;
+        }
+    }
+
+        public void MoveCube()
+    {
+        if (Direction_switcher == 0) {
+            Player.transform.Translate(0, 0, 2.45f);
+        }
+        else if (Direction_switcher == 1)
+        {
+
+            Player.transform.Translate(2.45f, 0,0 );
+        }
+        else if (Direction_switcher == 2)
+        {
+            Player.transform.Translate(0, 0, -2.45f);
+
+        }
+        else if (Direction_switcher == 3)
+        {
+
+            Player.transform.Translate(-2.45f, 0, 0);
+        }
+    }
+}
